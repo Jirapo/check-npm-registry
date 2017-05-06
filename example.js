01f1b19df8710565fc3fd0a279003ec1eddff700
Poling <466680197@qq.com>
2017-05-06 15:38:04 +0800

    add imagee

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ import request from './dist/request';
 import check from './dist';
 request('http://registry.npm.taobao.org/nowa')
   .then(function({ data }){
-    // console.log(data)
+    console.log(data)
   });
 
 check('http://registry.npm.taobao.org/').then(d => console.log(d));
